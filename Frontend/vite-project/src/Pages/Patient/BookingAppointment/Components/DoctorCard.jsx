bc3b0e533be9acf55dc492824be3ee92e0fb20cc
tausif900 <ansaritausif9268@gmail.com>
2026-04-23 17:28:38 +0530

    layout in progress

--- Frontend/vite-project/src/Pages/Patient/BookingAppointment/Components/DoctorCard.jsx
+++ Frontend/vite-project/src/Pages/Patient/BookingAppointment/Components/DoctorCard.jsx
@@ -2,62 +2,30 @@ import React from "react";
 
 const DoctorCard = () => {
   return (
-    <div className="px-1">
-      <div className="!sticky top-0 bg-slate-300 opacity-40 z-10 border-b-2">
-        <h1 className="!text-xl !font-semibold !font-mono !text-gray-800 !px-3 ">
-          Doctor Card
+    <div className="px-2">
+      <div>
+        <h1 className="!text-2xl !font-semibold !font-mono !text-black !px-3 !text-center">
+          Doctors List
         </h1>
-      </div>
-      <div className="card mb-2 mt-2">
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card’s content.
-          </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card mb-2">
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card’s content.
-          </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card mb-2">
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card’s content.
-          </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card mb-2">
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card’s content.
-          </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
+        <div className="p-2 rounded-2xl bg-green-200 border-gray-500 shadow-2xl drop-shadow-xl text-shadow-2xs">
+          <div className="flex justify-center items-center gap-3 border-b-2">
+            <div className="h-30 w-30 rounded-full overflow-hidden">
+              <img
+                className="object-cover"
+                src="https://png.pngtree.com/png-vector/20250415/ourmid/pngtree-female-doctor-portrait-in-white-png-image_15971053.png"
+              />
+            </div>
+            <div className="text-center">
+              <h3>Tausif Ansari</h3>
+              <p className="text-gray-600">specialist | 12 years experience</p>
+              <p className="bg-green-300 rounded-xl">Pediatrics</p>
+            </div>
+          </div>
+          <div className="flex justify-center mt-2">
+            <button className="bg-green-500 w-full px-2 py-2 !rounded-2xl font-semibold !text-xl active:scale-95">
+              Select Doctor
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- Frontend/vite-project/src/Pages/Patient/BookingAppointment/Components/DoctorList.jsx
+++ Frontend/vite-project/src/Pages/Patient/BookingAppointment/Components/DoctorList.jsx
@@ -5,7 +5,7 @@ const DoctorList = () => {
   return (
     <div
       id="doctorlist"
-      className="h-auto w-full bg-slate-300 rounded-xl overflow-y-auto"
+      className="h-auto w-full bg-green-300 rounded-xl overflow-y-auto"
     >
       <DoctorCard />
     </div>
