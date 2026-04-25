745d398748eb0be71354f50bdab426b9deee1089
tausif900 <ansaritausif9268@gmail.com>
2026-04-25 16:30:16 +0530

    done

--- Frontend/vite-project/src/Pages/Patient/BookingAppointment/Components/DoctorCard.jsx
+++ Frontend/vite-project/src/Pages/Patient/BookingAppointment/Components/DoctorCard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { FaStethoscope } from "react-icons/fa";
 
 const DoctorCard = ({ name, specialization, experience, image }) => {
   return (
     <div className="px-2 mt-2">
       <div>
-        <div className="p-2 mb-3 rounded-2xl border-gray-500 shadow-2xl drop-shadow-xl text-shadow-2xs hover:shadow-lg hover:scale-[1.01] transition">
+        <div className="p-2 mb-3 rounded-2xl border-gray-500 shadow-2xl drop-shadow-xl text-shadow-2xs">
           <div className="flex justify-center items-center gap-3 border-b-2">
             <div className="h-30 w-30 rounded-full overflow-hidden">
-              <img className="h-full w-full object-cover" src={image} />
+              <img className="h-full w-full object-cover md:shrink-0" src={image} />
             </div>
             <div className="text-center">
               <h3>{name}</h3>
@@ -17,9 +18,9 @@ const DoctorCard = ({ name, specialization, experience, image }) => {
               <p className="bg-green-300 rounded-xl">{specialization}</p>
             </div>
           </div>
-          <div className="flex justify-center mt-2">
-            <button className="bg-[#86f795] w-full px-2 py-2 !rounded-2xl font-semibold !text-xl active:scale-95">
-              Select Doctor
+          <div className="flex justify-center  mt-2">
+            <button className="bg-[#86f795] custom-btn flex justify-between items-center w-full px-2 py-2 !rounded-2xl font-semibold !text-xl active:scale-95 hover:bg-[#5bf56f]">
+              Select Doctor <FaStethoscope />
             </button>
           </div>
         </div>
